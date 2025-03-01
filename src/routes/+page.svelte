3079cfc8c1a808f785f6733b444c50d5b45f85dc
gerardamr <gerardmgamor@gmail.com>
2025-03-01 13:17:51 -0700

    gerard changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,62 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import { slide } from 'svelte/transition'
+
+	let busName;
+    let routes = [56, 201,523,9,901];
+    let showItems = false;
+    let i = $state(0);
+
+    function searchBus(){
+        showItems = true
+        for (let x= 0; x < routes.length; x++)
+            i++
+        return i
+    }
+</script>
+
+
+<!-- {#if $currentUser}
+    <h1>Seach</h1>
+{/if} -->
+
+
+
+<div class = "search-wrrapper">
+    <label id= "top">Search Bus </label>
+    <input type = "search" placeholder = "Enter Bus Number" bind:value={busName}>
+    
+</div>
+
+<button id = "search" on:click= {searchBus()}>Search</button>
+
+{#if showItems}
+	{#each routes.slice(0, i) as item}
+		<div id = "routes"transition:slide|global>
+			{item}
+		</div>
+	{/each}
+{/if}
+
+<style>
+    .search-wrapper{
+        margin-inline: auto;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding-top: 3rem;
+    }
+    #search{
+        margin-inline: auto;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        padding-top: 1;
+    }
+
+    #routes{
+		padding: 0.5em 0;
+		border-top: 1px solid #eee;
+        display: flex;
+
+    }
+</style>
---
+++ src/routes/SearchPage/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+	let busName;
+    let routes = [56, 201,523,9,901]
+    let showItems = true;
+    let i = 5;
+    
+
+    function searchBus(busName){
+        showItems = true
+        return 
+    }
+    
+
+</script>
+
+<style>
+  main {
+    font-family: sans-serif;
+    text-align: center;
+  }
+</style>
+{#if $currentUser}
+    <h1>Seach Bus</h1>
+{/if}
+
+{#if showItems}
+	{#each routes as item}
+		<div>
+			{item}
+		</div>
+	{/each}
+{/if}
+
+<div class = 'container'>
+    <h1>Search Bus</h1>
+</div>
+<button on:click= {() => showItems = true} type= "submit">Search</button>
