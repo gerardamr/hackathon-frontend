0370fb2eb49e6215966f089022a91127b48bd521
gerardamr <gerardmgamor@gmail.com>
2025-03-01 15:21:53 -0700

    help
    help

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,51 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    let busName= $state(["1","2","3","4"]);
+    let routes = $state([]);
+    let listlen = $state(routes.length);
+    let i = 0;
+    let showItems = $state(false);
+
+    function Bus(id, name, coordinate, routes) {
+        this.id = id;
+        this.Name = name;
+        this.coord = coordinate;
+        this.routes = routes;
+    }
+
+    function addBus(Bus){
+        busName.push(Bus)
+    }
+
+</script>
+
+
+
+<div class = "search-wrapper">
+    <label for='search'>Search Bus</label>
+    <input type ="search" id ="search"/>
+    <button on:click={() => showItems = true}>Search</button>
+</div>
+
+
+
+{#if showItems}
+	{#each busName.slice(0, listlen) as item}
+		<div id= 'searchResult'>
+			{item}
+		</div>
+	{/each}
+{/if}
+
+
+<style>
+    .search-wrapper{
+        margin-inline: auto;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding-top: 100px;
+    }
+    #searchResults{
+        bottom: 0
+    }
+</style>;
